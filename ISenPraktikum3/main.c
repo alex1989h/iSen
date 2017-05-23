403dfc11250dbda1fa975dfa9b571c96f4ab110d
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2017-05-23 08:58:36 +0200

    Neues Keil Projekt

---
+++ ISenPraktikum3/main.c
@@ -0,0 +1,62 @@
+/**
+  ******************************************************************************
+  * @file    	main.c 
+  * @author  	Alfred Lohmann
+  *        	  HAW-Hamburg
+  *          	Labor für technische Informatik
+  *          	Berliner Tor  7
+  *          	D-20099 Hamburg
+  * @version V1.0
+  * @date    23.05.2013
+  * @brief   Main program body
+  *******************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include "TI_Lib.h"
+#include "tft.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+
+
+/* Private typedef -----------------------------------------------------------*/
+
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+
+    
+  Init_TI_Board();
+    
+  printf("Hallo TI-Labor.\n\r");
+
+  TFT_cls();
+  TFT_puts("Hallo TI-Labor");
+  TFT_carriage_return();
+  TFT_newline();
+
+  return 0;
+
+}
+
+
+
+
