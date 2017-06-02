333ee984c3f5b89f3776438c996288f5a123d0ef
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2017-06-02 16:56:51 +0200

    Beschriftung der Grafen geändert

--- python/standard_deviation.py
+++ python/standard_deviation.py
@@ -1,7 +1,6 @@
 from pylab import *
 from numpy import genfromtxt
 import numpy as np
-#import math
 
 def quadrat(x):
   return x ** 2
@@ -33,22 +32,19 @@ d = genfromtxt('feature_gen_dataset1.csv', delimiter=',')
 #d = [3,4,1,10,11,20,5,6,7,8,20]
 subplot(p,1,1)
 plot(d[:,3])
-ylabel('Acceleration (m/s^2)')
-title('IMU Linear Acceleration Measurements')
+title('Fourth column of the data set referred to in Appendix A')
 grid(True)
 
 
 f = compute_standard_deviation(d[:,3],50)
 subplot(p,1,3)
 plot(f)
-ylabel('Acceleration (m/s^2)')
 title('Standard Deviation with buffer of 50')
 grid(True)
 
 f = compute_standard_deviation(d[:,3],300)
 subplot(p,1,5)
 plot(f)
-ylabel('Acceleration (m/s^2)')
 title('Standard Deviation with buffer of 300')
 grid(True)
 
